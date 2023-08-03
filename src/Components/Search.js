2f5faed4b257fbf6afd7274dedbde9a7312bf703
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-03 15:40:30 +0300

    restructure

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -11,40 +11,42 @@ function Search() {
   }, []);
 
   return (
-    <div className="row">
-      <div>
-        <input
-          type="search"
-          name="search"
-          placeholder="Search items here"
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-      </div>
-      {data.filter((row) =>{
-          if (search === "") {
-            return row;
-          } else if (
-            row.title.toLowerCase().includes(search.toLowerCase())
-          ) {
-            return row;
-          }
-        })
-        .map((row, i) => {
-          return (
-            <div className="card" key={i}>
-              <div className="image">
-                <img src={row.image} alt={row.image} />
-              </div>
-              <div className="title">
-                <h2>{row.title.substring(0, 20)}</h2>
-                <p>{row.price}</p>
-              </div>
-            </div>
-          );
-        })}
+    <div>
+        <div>
+            <input
+            type="search"
+            name="search"
+            placeholder="Search items here"
+            value={search}
+            onChange={(e) => {
+                setSearch(e.target.value);
+            }}
+            />
+        </div>
+        <div class="card-group">
+        {data.filter((row) =>{
+            if (search === "") {
+                return row;
+            } else if (
+                row.title.toLowerCase().includes(search.toLowerCase())
+            ) {
+                return row;
+            }
+            })
+            .map((row, i) => {
+            return (
+                <div >
+                    <div class="card" key={i}>
+                        <img class="card-img-top" src={row.image} alt={row.image} />
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{row.title.substring(0, 20)}</h5>
+                            <p class="card-text">{row.price}</p>
+                    </div>
+                </div>
+            );
+            })}
+        </div>
     </div>
   );
 }
