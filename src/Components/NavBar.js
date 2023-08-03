60fca984f928126b9e2ac54b192c8cf9e55aa1e0
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-03 20:28:58 +0300

    Completed logo for Shooping app

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">
-            <img src="/logo.png" alt="logo" />
+            <img src="/logo2.png" alt="logo" />
           </NavLink>
           <div className="navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
