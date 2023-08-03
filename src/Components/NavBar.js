cd50c64c4d0ab06ca5aad5c6d628db80973dc66f
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-03 14:42:04 +0300

    Completed logo for Shooping app

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,25 +7,33 @@ function NavBar() {
 
   return (
     <div>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-        <h6 className="navbar-brand" >Navbar</h6>
-        <div className="navbar-collapse" id="navbarNav">
+          <NavLink className="navbar-brand" to="/">
+            <img src="/logo.png" alt="logo" />
+          </NavLink>
+          <div className="navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-            <li className="nav-item" >
+              <li className="nav-item">
                 <NavLink to="/">
-                    <i className="bi bi-house p-2 " style={{fontSize: "2rem", color: "black" }}></i>
+                  <i
+                    className="bi bi-house p-2"
+                    style={{ fontSize: "2rem", color: "black" }}
+                  ></i>
                 </NavLink>
-            </li>
-            <li className="nav-item">
-                    <NavLink to="/Login">
-                        <i className="bi bi-person-circle  p-2" style={{fontSize: "2rem", color: "black" }}></i>
-                    </NavLink>
-            </li>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/Login">
+                  <i
+                    className="bi bi-person-circle p-2"
+                    style={{ fontSize: "2rem", color: "black" }}
+                  ></i>
+                </NavLink>
+              </li>
             </ul>
+          </div>
         </div>
-        </div>
-    </nav>
+      </nav>
     </div>
   );
 }
