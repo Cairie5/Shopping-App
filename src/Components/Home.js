8df48eb5f25e6ba8425c52257f3865c0a2046d9d
austinaeden <austin.mbogo@student.moringaschool.com>
2023-08-03 12:59:22 +0300

    cart update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,27 +1,61 @@
 import React, {useEffect, useState} from "react";
-import Search from "./Search";
+import SortBar from "./SortBar";
 import ProductList from "./ProductList"
+import Search from "./Search";
 import Cart from "./Cart";
 
 function Home () {
-
     const[product, setProduct] = useState([])
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedPrice, setSelectedPrice] = useState("");
     const[item, setItem] = useState([])
     
 
-    useEffect (() =>{
-        fetch('https://fakestoreapi.com/products')
-        .then((res) => res.json())
-        .then (data => {
-            setProduct(data)
-        })
-    }, []);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+          .then((res) => res.json())
+          .then((data) => {
+            setProducts(data);
+            const uniqueCategories = [...new Set(data.map((item) => item.category))];
+            setCategories(uniqueCategories);
+          });
+      }, []);
+
+      const handleCategoryChange = (category) => {
+        setSelectedCategory(category);
+      };
+    
+      const handlePriceChange = (price) => {
+        setSelectedPrice(price);
+      };
+    
+      const filteredProducts = products.filter((product) => {
+        if (selectedCategory && product.category !== selectedCategory) {
+          return false;
+        }
+        return true;
+      });
+    
+      const sortedProducts = selectedPrice
+        ? filteredProducts.slice().sort((a, b) => {
+            return (
+              selectedPrice === "low" ? a.price - b.price : b.price - a.price
+            );
+          })
+        : filteredProducts;
 
     return (
         <div>
             <i className="bi bi-cart-fill position-absolute top-0 end-0 m-4 " style={{fontSize: "2rem", color: "cornflowerblue", cursor:"pointer" }}   onClick={() => {const cart = document.querySelector(".cart");cart.classList.add("active");}}></i>
             <h2>PRODUCTS</h2>
             <Search />
+            <SortBar
+                categories={categories}
+                onCategoryChange={handleCategoryChange}
+                onPriceChange={handlePriceChange}
+            />
             <ProductList products={product} className="shop-content" item={item} setItem={setItem}/>
             <Cart item={item} setItem={setItem} />
         </div>
