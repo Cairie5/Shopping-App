7c14fbde167de436ab77a2496815ab0a7ef31b64
austinaeden <austin.mbogo@student.moringaschool.com>
2023-08-04 14:19:49 +0300

    profile search and about update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -22,25 +22,41 @@ function Login() {
   };
 
   return (
-    <div>
+    <div className="profile"> 
       <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-      <p>
-        Don't have an account? <Link to="/signup">Signup</Link> {/* Add the Signup link */}
-      </p>
-      {error && <p>{error}</p>}
+      <div className="p-5 ">
+        <form className="rounded border border-primary-subtle p-4 bg-dark text-white">
+          <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <input 
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Email"
+              value={email}
+              aria-describedby="emailHelp"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="form-control" 
+              id="exampleInputPassword1"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" onClick={handleLogin}>Login</button>
+          <p>
+            Don't have an account? <Link to="/signup">Signup</Link> {/* Add the Signup link */}
+          </p>
+          {error && <p>{error}</p>}
+        </form>
+      </div>
     </div>
   );
 }
