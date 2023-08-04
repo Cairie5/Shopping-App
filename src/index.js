c6ebc0c6fdc0665819d54a780d183e53b29d0fad
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-04 12:41:30 +0300

    fixed errors

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import App from "./Components/App";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+// Use createRoot to render the App component
+const root = document.getElementById("root");
+const rootContainer = createRoot(root);
+
+rootContainer.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
